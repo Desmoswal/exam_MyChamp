18099b370374bf2515abb520be385812bf10bd0a
Desmoswal <mark227a@easv365.dk>
2017-01-13 11:02:44 +0100

    Ranking List working

--- MyChamp/src/myChamp/GUI/Controller/RankingListController.java
+++ MyChamp/src/myChamp/GUI/Controller/RankingListController.java
@@ -7,11 +7,16 @@ package myChamp.GUI.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import myChamp.BE.Group;
+import myChamp.BE.Groups;
 import myChamp.BE.Team;
+import myChamp.GUI.Model.MyChampModel;
 
 /**
  * FXML Controller class
@@ -43,14 +48,72 @@ public class RankingListController implements Initializable {
     @FXML
     private TableColumn<Team, String> colTeamsGroupB;
     @FXML
-    private TableColumn<Team, String> colPoinstGroupB;
+    private TableColumn<?, ?> colPointsGroupB;
+    
+    
+    private MyChampModel model = new MyChampModel();
+    
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        
+        for (Group group : model.getGroups()) {
+            setTableProperties(group);
+            setTableItems(group);
+        }
+        
+        
+    }
+ 
+    /**
+     * Sets table colmn properies for a specified group's table
+     * @param group 
+     */
+    private void setTableProperties(Group group) {
+        switch(group.getName()) {
+            case "a":
+                colTeamsGroupA.setCellValueFactory(new PropertyValueFactory("name"));
+                colPointsGroupA.setCellValueFactory(new PropertyValueFactory("score"));
+                
+                colPointsGroupA.setSortable(true);
+                colPointsGroupA.setSortType(TableColumn.SortType.ASCENDING);
+                break;
+            case "b":
+                colTeamsGroupB.setCellValueFactory(new PropertyValueFactory("name"));
+                colPointsGroupB.setCellValueFactory(new PropertyValueFactory("score"));
+                break;
+            case "c":
+                colTeamsGroupC.setCellValueFactory(new PropertyValueFactory("name"));
+                colPointsGroupC.setCellValueFactory(new PropertyValueFactory("score"));
+                break;
+            case "d":
+                colTeamsGroupD.setCellValueFactory(new PropertyValueFactory("name"));
+                colPointsGroupD.setCellValueFactory(new PropertyValueFactory("score"));
+                break;
+        }
+    }
     
+    /**
+     * Sets the table items for a specified group's table.
+     * @param group 
+     */
+    private void setTableItems(Group group) {
+        switch(group.getName()) {
+                case "a":
+                    tblGroupA.setItems(FXCollections.observableArrayList(model.getGroups().get(model.getGroups().indexOf(group)).getTeams()));
+                    break;
+                case "b":
+                    tblGroupB.setItems(FXCollections.observableArrayList(model.getGroups().get(model.getGroups().indexOf(group)).getTeams()));
+                    break;
+                case "c":
+                    tblGroupC.setItems(FXCollections.observableArrayList(model.getGroups().get(model.getGroups().indexOf(group)).getTeams()));
+                    break;
+                case "d":
+                    tblGroupD.setItems(FXCollections.observableArrayList(model.getGroups().get(model.getGroups().indexOf(group)).getTeams()));
+                    break;
+        }
+    }
 }
